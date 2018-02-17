371f07a77bedfd8fb80e174f74a2f265d5838c33
Alex Chojnacki <thealex@umich.edu>
2018-02-17 11:34:43 -0500

    Created database schema and save methods

---
+++ database/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/fetcher';
+mongoose.connect(MONGODB_URI);
+
+let cardSchema = mongoose.Schema({
+  url: {type: String, unique: true, required: true, dropDups: true},
+  labels: [
+    {
+      name: String,
+      score: Number,
+    },
+  ],
+});
+
+let Card = mongoose.model('Card', cardSchema);
+
+// TODO: Cleanup/simplify
+let save = (cardData, callback) => {
+  let card = new Card(cardData);
+  card.save((err, documents) => {
+    if (err) callback(err, null);
+    else callback(null, documents);
+  });
+};
+
+const retrieve = callback => {
+  let query = Card.find();
+  query.exec((err, repos) => {
+    if (err) callback(err, null);
+    else callback(null, repos);
+  });
+};
+
+module.exports.save = save;
+module.exports.retrieve = retrieve;
